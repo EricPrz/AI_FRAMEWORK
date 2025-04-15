Conv2dC forward fully working and fixed, cannot say the same for gradient

NN/PY/framework.py
@@ -89,22 +89,42 @@ class Tensor(object):
 
             """
 
-            init_time = time.time()
-            conv = self.creators[0]
+            # init_time = time.time()
+            # conv = self.creators[0]
+            # x = self.creators[1]
+            #
+            # new_gradient = np.zeros_like(x.data)
+            # base_idxs = np.arange(x.data.shape[2] * x.data.shape[3]).reshape((x.data.shape[2], x.data.shape[3]))
+            #
+            # for i in range(conv.n ** 2):
+            #     coords = conv.extracted_from_pos[i]
+            #     idxs = base_idxs[coords["y_from"]:coords["y_to"]:coords["step"], coords["x_from"]:coords["x_to"]:coords["step"]].flatten()
+            #
+            #     row_idxs, col_idxs = np.unravel_index(idxs, (x.shape[2], x.shape[3]))
+            #
+            #     new_gradient[:, :, row_idxs, col_idxs] += self.gradient.data.reshape((x.shape[0], conv.in_channels, conv.kernel_size * conv.kernel_size, conv.n * conv.n))[:, :, :, i]
+            # #print("Sects Back Time:", time.time() - init_time)
+            # x.backward(Tensor(new_gradient))
+
+            sect = self.creators[0]
             x = self.creators[1]
 
-            new_gradient = np.zeros_like(x.data)
-            base_idxs = np.arange(x.data.shape[2] * x.data.shape[3]).reshape((x.data.shape[2], x.data.shape[3]))
-           
-            for i in range(conv.n ** 2):
-                coords = conv.extracted_from_pos[i]
-                idxs = base_idxs[coords["y_from"]:coords["y_to"]:coords["step"], coords["x_from"]:coords["x_to"]:coords["step"]].flatten()
-               
-                row_idxs, col_idxs = np.unravel_index(idxs, (x.shape[2], x.shape[3]))
-            
-                new_gradient[:, :, row_idxs, col_idxs] += self.gradient.data.reshape((x.shape[0], conv.in_channels, conv.kernel_size * conv.kernel_size, conv.n * conv.n))[:, :, :, i]
-            #print("Sects Back Time:", time.time() - init_time)
-            x.backward(Tensor(new_gradient))
+            new_gradient = np.zeros((x.data.shape), dtype=np.float32)
+
+            lib.setConvGrad(self.gradient.data.ctypes.data_as(ctypes.POINTER(ctypes.c_float)), 
+                            new_gradient.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+                            sect.pos.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
+                            sect.N,
+                            sect.chs,
+                            sect.n,
+                            sect.dims,
+                            sect.kernel_size)
+
+            tns_ng = Tensor(new_gradient)
+            del (new_gradient)
+
+            x.backward(tns_ng)
+
 
 
 